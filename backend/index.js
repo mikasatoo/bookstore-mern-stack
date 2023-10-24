59983d65b93f84489fd6e11d4547d8b71bf0a7d5
Mika Sato <mika99@live.com.au>
2023-10-24 12:15:28 +1000

    create first HTTP route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send('Welcome to the MERN Stack Tutorial');
+});
+
 app.listen(PORT, (err) => {
     if (err) console.log("Error in server setup");
     console.log(`App is listening on port: ${PORT}`);
