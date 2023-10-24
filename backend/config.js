d1895d952f9517147c941cd366bc53b9b953c63d
Mika Sato <mika99@live.com.au>
2023-10-24 12:40:18 +1000

    connect MongoDB database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://mika99:z9MGy9uOLYDy2Tci@bookstore-mern.r42kd3g.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -8,7 +9,15 @@ app.get('/', (req, res) => {
     return res.status(234).send('Welcome to the MERN Stack Tutorial');
 });
 
-app.listen(PORT, (err) => {
-    if (err) console.log("Error in server setup");
-    console.log(`App is listening on port: ${PORT}`);
-});
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, (err) => {
+            if (err) console.log("Error in server setup");
+            console.log(`App is listening on port: ${PORT}`);
+        });        
+    })
+    .catch((error) => {
+        console.log(error);
+    });
