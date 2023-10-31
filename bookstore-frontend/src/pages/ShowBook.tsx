8b0adef4ca7375070b488fa69b881a677437b527
Mika Sato <mika99@live.com.au>
2023-10-31 17:02:47 +1000

    add book title to h1

--- bookstore-frontend/src/pages/ShowBook.tsx
+++ bookstore-frontend/src/pages/ShowBook.tsx
@@ -42,7 +42,9 @@ const ShowBook = () => {
     return (
         <div className='p-4'>
             <BackButton />
-            <h1 className='text-3xl my-4'>Show Book</h1>
+            <h1 className='text-3xl my-4'>
+                Show Book: {book.title}
+            </h1>
 
             {loading ? (
                 <Spinner />
